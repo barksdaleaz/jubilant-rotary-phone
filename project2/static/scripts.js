988183707fd47f4c2061c11aac03e6717071fd28
barksdaleaz <59379235+barksdaleaz@users.noreply.github.com>
2020-05-05 15:38:04 -0500

    current updates 5/5/2020

--- project2/static/scripts.js
+++ project2/static/scripts.js
@@ -1,113 +1,116 @@
-//if (!localStorage.getItem('username') && !localStorage.getItem('current_channel')) {
-  //var username = prompt('Enter a username');
-  //localStorage.setItem('username', username)
-  //localStorage.setItem('current_channel', )
-//}
-
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Connect to websocket
-  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+    // Connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-  // Get user's local localStorage
-  var my_storage = window.localStorage;
-  // var username = '<%= Session["username"] %>';
-  // var username = document.querySelector("#username");
-  var username = localStorage.setItem('username', 'testuser');
-  //localStorage.setItem('username', username);
-  //document.querySelector('#user').innerHTML = `Heyyy, ${username}!`;
-  //localStorage.setItem('username', username);
+    // Get user's local localStorage
+    var my_storage = window.localStorage;
 
-  // When connected, configure buttons
-  socket.on('connect', () => {
-    function joinchannel() {
-      // tell people someone joined
-      socket.emit('joined', current_channel);
-    }
-    var leave_btn = document.getElementById('leave');
-    if (leave_btn){
-      // tell people when someone left and remove the last channel from that user
-      leave_btn.addEventListener('click', () => {
-        var room_to_leave = localStorage.getItem('current_channel');
-        socket.emit('left', room_to_leave);
-        localStorage.removeItem('last_channel');
-      });
+    var add_user_btn = document.getElementById('lets-go');
+    if (add_user_btn){
+      document.querySelector('#lets-go').onclick = () => {
+        const username = document.querySelector('#username').value;
+        localStorage.setItem('username', username);
+        alert(`Welcome, ${username}!`)
+        console.log("the user has been set.")
+      };
     };
+    var user = localStorage.getItem('username')
+    console.log(user)
+
+    //var cur_chan = document.getElementById('ch-name').value;
+    //localStorage.setItem('current_channel', cur_chan);
+    //var current_channel = localStorage.getItem('current_channel');
+    // When connected, configure buttons
+    socket.on('connect', () => {
+      //function joinchannel() {
+      //  console.log('Made it here')
+        // tell people someone joined
+        //socket.emit('joined', current_channel);
+      //}
+      var leave_btn = document.getElementById('leave');
+      if (leave_btn){
+        // tell people when someone left and remove the last channel from that user
+        leave_btn.onclick = () => {
+          socket.emit('left');
+          //localStorage.setItem('current_channel', current_channel);
+          //localStorage.removeItem('last_channel');
+          console.log("someone left")
+        };
+      };
+    }); // end of socket.on('connect')
 
     // similar to above with clicking Log Out
     var logout_btn = document.getElementById('logout');
     if (logout_btn){
-      document.querySelector('#logout').addEventListener('click', () => {
-        localStorage.removeItem('last_channel');
-      });
+        document.querySelector('#logout').addEventListener('click', () => {
+          //localStorage.removeItem('last_channel');
+        });
+      };
+
+      // sending messages with a timestamp in the message field
+    var send_btn = document.querySelector('#send-button');
+    if (send_btn){
+      send_btn.onclick = () => {
+
+          // get the message that they wrote
+          var message_content = document.querySelector('#message-input').value;
+          // save time
+          var tstmp = new Date();
+          timestp = tstmp.toLocaleString('en-US');
+          timestamp = timestp.replace(/:\d+ /, ' ');
+          var user = localStorage.getItem('username')
+          var thechannel = document.querySelector('#thechannel').value;
+          //var current_channel = localStorage.getItem('current_channel');
+
+          // put all the pieces of the message into one variable
+          let message_data = {"message_content": message_content,
+                              "timestamp": timestamp,
+                              "user": user,
+                              "current_channel": thechannel};
+          console.log(message_data);
+          socket.emit('send message', message_data);
+          // clear the input field
+          document.querySelector('#message-input').value = '';
+          event.preventDefault();
+          return false;
+
+        };// end of send button
     };
 
-    // sending messages with a timestamp in the message field
-    document.querySelector('#send-button').addEventListener('click', function(event){
-      event.preventDefault();
+    // using Enter key as a "send", doesn't quite work yet
+    //document.querySelector('#message-input').addEventListener("keydown", event => {
+      //event.preventDefault();
+      //if (event.key == "Enter") {
+          //document.getElementById('send-button').click();
+      //};
+    //});
 
-      // get the message that they wrote
-      var message_content = document.querySelector('#message-input').value
-      // save time
-      var timestamp = new Date();
-      timestamp = timestamp.toLocaleString('en-US');
-      var user = username;
-      var current_channel = localStorage.getItem('current_channel');
-      // put all the pieces of the message into one variable
-      let message_data = {"message_content": message_content,
-                          "timestamp": timestamp};
-                          //"user": user,
-                          //"current_channel": current_channel};
-
-      socket.emit('send message', message_data);
-      console.log(message_data);
-      // clear the input field
-      document.querySelector('#message-input').value = '';
-      return false;
+    socket.on('status', data => {
+      console.log('Recieved status');
+      var message_list = document.getElementById('comment-list');
+      const leftli = document.createElement('li');
+      leftli.innerHTML = `${data.msg}`;
+      message_list.append(leftli);
+      localStorage.setItem('last_channel', data.channel)
     });
-  });
 
-  socket.on('status', data => {
-    console.log('Recieved status');
-    let row = '<' + `${data.msg}` + '>';
-    document.querySelector('#chat').value += row + '\n';
-    localStorage.setItem('last_channel', data.channel)
-  });
+    socket.once('announce message', message_data => {
+      console.log("inside announce message")
+      //event.preventDefault();
+      var message_list = document.getElementById('comment-list');
+      const li = document.createElement('li');
+      li.innerHTML = `<div>
+                        <li><b>${message_data["user"]}:</b> ${message_data["message_content"]}</li>
+                      </div>
+                      <div>
+                        <li><small class="text-muted">${message_data["timestamp"]}</small></li>
+                      </div>`;
+      message_list.append(li);
+      li.scrollIntoView();
+    });
 
-  socket.on('announce message', message_data => {
-
-    const li = document.createElement('li');
-    li.setAttribute('class', 'media comment-item');
-
-    const div_media_body = document.createElement('div');
-    div_media_body.setAttribute('class', 'media-body comment-media');
-
-    const h5 = document.createElement('h5');
-    h5.setAttribute('class', 'mt-0 mb-1 comment-user');
-    let midpt = '&middot';
-    let space = '\u0020';
-    h5.innerHTML = `the user's name here${space}${midpt}${space}`;
-    //h5.innerHTML = `${message_data["user"]}${space}${midpt}${space}`;
-
-    const div_time = document.createElement('div');
-    div_time.setAttribute('class', 'comment-time');
-    div_time.setAttribute('id', 'time');
-    div_time.innerHTML = message_data["timestamp"];
-
-    const div_comment = document.createElement('div');
-    div_comment.setAttribute('class', 'comment-comment');
-    div_comment.innerHTML = message_data["message_content"];
-
-    div_media_body.appendChild(h5);
-    div_media_body.appendChild(div_time);
-    div_media_body.appendChild(div_comment);
-
-    //li.appendChild(div_media_body);
-    li.innerHTML = `this is a test`;
-
-    document.querySelector('#comment-list').append(li);
-    li.scrollIntoView();
-  });
-
-
+    $(document).ready(function() {
+      $(".dropdown-toggle").dropdown();
+    });
 });
